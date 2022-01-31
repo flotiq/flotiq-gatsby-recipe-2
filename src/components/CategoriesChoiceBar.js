9fdbf802d638b35863f8a64ef395aa9066e21829
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-01-31 17:08:45 +0100

    WIP: Category choice bar component - widthn adjustment

--- src/components/CategoriesChoiceBar.js
+++ src/components/CategoriesChoiceBar.js
@@ -15,8 +15,8 @@ const CategoriesChoiceBar = ({ additionalClass }) => {
     }
 
     return (
-        <div className="w-full md:max-w-3xl flex justify-between mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="sm:hidden">
+        <div className="md:max-w-3xl flex justify-between mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="sm:hidden w-full">
                 <label htmlFor="tabs" className="sr-only">
                     Select a tab
                 </label>
