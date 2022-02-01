90a1b5f4f5840ed8b804659f9f22deceff8d8d2f
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-01 13:51:49 +0100

    category tabs variables moved outside component

--- src/components/CategoriesChoiceBar.js
+++ src/components/CategoriesChoiceBar.js
@@ -1,23 +1,16 @@
 import React from 'react';
 
-const CategoriesChoiceBar = ({ additionalClass }) => {
-    const tabs = [
-        { name: 'Breakfast', href: '#', current: true },
-        { name: 'Dinner', href: '#', current: false },
-        { name: 'Dessert', href: '#', current: false },
-        { name: 'Lunch', href: '#', current: false },
-        { name: 'Snack', href: '#', current: false },
-        { name: 'Vegan', href: '#', current: false },
-    ];
-
+const CategoriesChoiceBar = ({ additionalClass, categoryTabs }) => {
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ');
     }
 
     return (
-        <div className={['md:max-w-3xl flex justify-between mx-auto px-2 sm:px-6 lg:px-8', ...additionalClass].join(' ')}>
+        <div className={['md:max-w-3xl flex justify-between '
+        + 'mx-auto px-2 sm:px-6 lg:px-8', ...additionalClass].join(' ')}
+        >
             <div className="sm:hidden w-full">
-                <label htmlFor="tabs" className="sr-only">
+                <label htmlFor="categoryTabs" className="sr-only">
                     Select a tab
                 </label>
                 {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
@@ -26,9 +19,9 @@ const CategoriesChoiceBar = ({ additionalClass }) => {
                     name="tabs"
                     className="block w-full pl-3 pr-10 py-2 text-base border-gray-300
                     focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                    defaultValue={tabs.find((tab) => tab.current).name}
+                    defaultValue={categoryTabs.find((tab) => tab.current).name}
                 >
-                    {tabs.map((tab) => (
+                    {categoryTabs.map((tab) => (
                         <option key={tab.name}>{tab.name}</option>
                     ))}
                 </select>
@@ -36,7 +29,7 @@ const CategoriesChoiceBar = ({ additionalClass }) => {
             <div className="w-full hidden sm:block">
                 <div className="border-b border-primary/25 ">
                     <nav className="w-full -mb-px flex justify-between" aria-label="Tabs">
-                        {tabs.map((tab) => (
+                        {categoryTabs.map((tab) => (
                             <a
                                 key={tab.name}
                                 href={tab.href}
--- src/templates/index.js
+++ src/templates/index.js
@@ -6,12 +6,20 @@ import CategoriesChoiceBar from '../components/CategoriesChoiceBar';
 
 const IndexPage = ({ data }) => {
     const recipes = data.allRecipe.nodes;
+    const categoryTabs = [
+        { name: 'Breakfast', href: '#', current: true },
+        { name: 'Dinner', href: '#', current: false },
+        { name: 'Dessert', href: '#', current: false },
+        { name: 'Lunch', href: '#', current: false },
+        { name: 'Snack', href: '#', current: false },
+        { name: 'Vegan', href: '#', current: false },
+    ];
     return (
         <Layout>
             <Helmet>
                 <title>Flotiq Gatsby recipe starter</title>
             </Helmet>
-            <CategoriesChoiceBar additionalClass={['my-5']} />
+            <CategoriesChoiceBar additionalClass={['my-5']} categoryTabs={categoryTabs} />
             {recipes.map((recipe) => (
                 <a href={`/${recipe.slug}`}><p key={recipe.id}>{recipe.name}</p></a>
             ))}
