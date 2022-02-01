73564d462a74516528c5ab9f5d0931e6d7dcef32
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-01 22:35:19 +0100

    recipe back button component build

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/layout';
+import RecipeBackButton from '../components/recipe/RecipeBackButton';
 
 const RecipeTemplate = ({ data }) => {
     const { recipe } = data;
     return (
-        <Layout additionalClass={['bg-white']}>
-            <div>Recipe</div>
+        <Layout additionalClass={['bg-light-gray']}>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <RecipeBackButton additionalClass={['mt-12 mb-5 uppercase']} backButtonText="Go back" />
+            </div>
         </Layout>
     );
 };
