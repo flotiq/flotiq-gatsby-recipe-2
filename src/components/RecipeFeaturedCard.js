62590b368d1ae8d95e8de339bd11189ed5df870d
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-01 15:53:51 +0100

    WIP: recipe card component build #2

--- src/components/RecipeFeaturedCard.js
+++ src/components/RecipeFeaturedCard.js
@@ -1,8 +1,58 @@
 import React from 'react';
 
-const RecipeFeaturedCard = () => (
-    <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
-        Featured recipe
+const RecipeFeaturedCard = ({ preparationTime, portions, title, tags, image }) => (
+    <div className="max-w-7xl mx-auto flex flex-wrap lg:flex-nowrap align-start bg-white
+                overflow-hidden mb-4 cursor-pointer"
+    >
+        <div className="flex flex-col items-start justify-between basis-full lg:basis-2/5
+        order-2 lg:order-1 px-5 md:px-10 pt-10 pb-5"
+        >
+            <div className="flex flex-wrap justify-between text-sm font-light mt-5">
+                <p className="mr-5">
+                    Time:
+                    {' '}
+                    {preparationTime}
+                </p>
+                <p>
+                    Portions:
+                    {' '}
+                    {portions}
+                </p>
+            </div>
+            <div>
+                <h1 className="text-3xl font-semibold leading-10">
+                    {title}
+                </h1>
+                <p className="text-base font-light my-5">
+                    excerpt
+                </p>
+            </div>
+            <div className="w-full">
+                <div className="flex flex-wrap justify-start text-sm font-light mt-5 space-x-7">
+                    {tags && tags.map((tag) => (
+                        <a
+                            href="/"
+                            className="mx-1 my-1 py-2 inline-flex items-center justify-center
+                        rounded-md underline text-sm font-light hover:text-secondary"
+                        >
+                            {tag}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+        <div
+            className="basis-full lg:basis-3/5 order-1 lg:order-2 bg-no-repeat bg-cover bg-center bg-secondary"
+            style={{ backgroundImage: `url('${image}')` }}
+        >
+            {image && (
+                <img
+                    className="block lg:hidden w-full"
+                    src={image}
+                    alt={title}
+                />
+            )}
+        </div>
     </div>
 );
 
--- src/templates/index.js
+++ src/templates/index.js
@@ -3,18 +3,33 @@ import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import Layout from '../layouts/layout';
 import RecipeFeaturedCard from '../components/RecipeFeaturedCard';
+import RecipeImage from '../assets/recipe-image.jpg';
 
 const IndexPage = ({ data }) => {
     const recipes = data.allRecipe.nodes;
     return (
-        <Layout>
+        <Layout additionalClass={['bg-light-gray']}>
             <Helmet>
                 <title>Flotiq Gatsby recipe starter</title>
             </Helmet>
             {recipes.map((recipe) => (
                 <a href={`/${recipe.slug}`}><p key={recipe.id}>{recipe.name}</p></a>
             ))}
-            <RecipeFeaturedCard />
+            <RecipeFeaturedCard
+                title={(
+                    <span className="block text-2xl md:text-4xl mb-4 mx-1 font-normal leading-normal">
+                        Cozy, Little
+                        <span className="block text-secondary"> Chilli Weekend </span>
+                    </span>
+                )}
+                excerpt="If you’re a beginning website owner you might be wondering what a
+            CMS is and if you need one. This article will look at some valid reasons to invest in a CMS,
+            but also will try to shed some light on the caveats, so read on."
+                tags={['#dinner', '#vegan', '#lunch', '#glutenfree']}
+                preparationTime="10 min"
+                portions="2"
+                image={RecipeImage}
+            />
         </Layout>
     );
 };
