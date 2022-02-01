7909a4d43218ee5ab9853e988f309fee08f2317b
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-01 23:04:19 +0100

    recipe header image component build

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Image } from 'flotiq-components-react';
 import Layout from '../layouts/layout';
 
 const RecipeTemplate = ({ data }) => {
     const { recipe } = data;
     return (
-        <Layout additionalClass={['bg-white']}>
-            <div>Recipe</div>
+        <Layout additionalClass={['bg-light-gray']}>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 bg-white">
+                <div className="flex basis-full md:basis-1/2">
+                    <Image url={recipe.image[0] && recipe.image[0].localFile.publicURL} />
+                </div>
+                <div className="flex flex-col basis-full md:basis-1/2 pl-0 md:pl-12 py-5">
+                    <div>Recipe description</div>
+                </div>
+            </div>
         </Layout>
     );
 };
