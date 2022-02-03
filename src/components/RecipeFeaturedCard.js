773474774734a89c2eb557756e8411996ea5d3b3
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-02-03 12:19:54 +0100

    recipe featured card component refactor due to flotiq component use

--- src/components/RecipeFeaturedCard.js
+++ src/components/RecipeFeaturedCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Card } from 'flotiq-components-react';
 
-const RecipeFeaturedCard = ({ preparationTime, portions, title, excerpt, tags, image }) => (
-    <div className="max-w-7xl mx-auto flex flex-wrap lg:flex-nowrap align-start bg-white
-                overflow-hidden mb-4 cursor-pointer"
+const CustomRecipeFeaturedCard = ({ preparationTime, portions, title, excerpt, tags, image, onClick }) => (
+    <Card
+        vertical
+        onClick={onClick}
+        bordered={false}
+        rounded="none"
+        additionalClasses={['mb-4 cursor-pointer max-w-7xl mx-auto ']}
+        proportionsForVertical={{
+            body: '2/5',
+            img: '3/5',
+            breakpoint: 'lg',
+        }}
     >
-        <div className="flex flex-col items-start justify-between basis-full lg:basis-2/5
-        order-2 lg:order-1 px-5 md:px-10 py-5"
+        <Card.Body
+            additionalClasses={[
+                'flex flex-col items-start justify-between '
+                + 'order-2 lg:order-1 px-5 md:px-10 pt-10 pb-5',
+            ]}
         >
-            <div className="flex flex-wrap justify-between text-sm font-light space-x-5">
+            <div className="flex flex-wrap justify-between text-sm font-light space-x-5 mb-14">
                 <p className="px-4 py-3 bg-light-gray">
                     Time:
                     {' '}
@@ -19,13 +32,13 @@ const RecipeFeaturedCard = ({ preparationTime, portions, title, excerpt, tags, i
                     <span className="font-semibold">{portions}</span>
                 </p>
             </div>
-            <div className="pt-10 pb-5">
-                <h1 className="font-semibold leading-10">
+            <div>
+                <Card.Title>
                     {title}
-                </h1>
-                <p className="text-base font-light my-5">
+                </Card.Title>
+                <Card.Text additionalClasses={['line-clamp-5 lg:line-clamp-4 xl:line-clamp-5']}>
                     {excerpt}
-                </p>
+                </Card.Text>
             </div>
             <div className="w-full">
                 <div className="flex flex-wrap justify-start text-sm font-light mt-5">
@@ -33,27 +46,22 @@ const RecipeFeaturedCard = ({ preparationTime, portions, title, excerpt, tags, i
                         <a
                             href="/"
                             className="mr-7 my-1 py-2 inline-flex items-center justify-center
-                        rounded-md underline text-sm font-light hover:text-secondary"
+                    rounded-md underline text-sm font-light hover:text-secondary"
                         >
                             {tag}
                         </a>
                     ))}
                 </div>
             </div>
-        </div>
+        </Card.Body>
         <div
-            className="basis-full lg:basis-3/5 order-1 lg:order-2 bg-no-repeat bg-cover bg-center bg-secondary"
             style={{ backgroundImage: `url('${image}')` }}
+            className="w-full lg:w-auto order-1 lg:order-2 lg:basis-3/5 bg-cover bg-center"
         >
-            {image && (
-                <img
-                    className="block lg:hidden w-full"
-                    src={image}
-                    alt={title}
-                />
-            )}
+            <Card.Img src={image} alt={title} additionalClasses={['block lg:hidden w-full']} />
         </div>
-    </div>
+
+    </Card>
 );
 
-export default RecipeFeaturedCard;
+export default CustomRecipeFeaturedCard;
