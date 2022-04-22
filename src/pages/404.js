132d3e5d61f19ebb2964e8199f1a2e5d555d9995
Karol Grochowalski <karol.grochowalski@gmail.com>
2022-04-22 13:20:53 +0200

    404 more styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,12 +9,16 @@ const NotFoundPage = () => (
         <Helmet>
             <title>Page not found</title>
         </Helmet>
-        <Header additionalClasses={['text-center', 'mt-20']}>
-            Page not found, sorry
-        </Header>
-        <div className="text-center mt-5 pt-5">
-            {/* Example usage of button */}
-            <Link to="/"><Button label="Go back to index" /></Link>
+        <div className="py-32">
+            <Header additionalClasses={['basis-1 mb-10 !text-5xl !font-medium text-center']}>
+                Page not found, sorry
+            </Header>
+            <div className="text-center mt-5 pt-5">
+                {/* Example usage of button */}
+                <Link to="/">
+                    <Button label="Go back to index" />
+                </Link>
+            </div>
         </div>
     </Layout>
 );
